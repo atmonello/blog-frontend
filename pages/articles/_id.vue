772424ce38561f1d44f50e014525a10c589136f8
Andre Monello <atmonello@outlook.com>
2021-02-04 00:30:12 -0300

    (feat) articles: add articles page

--- pages/articles/_id.vue
+++ pages/articles/_id.vue
@@ -1,6 +1,16 @@
 <template>
   <v-container>
-    <div v-html="this.$md.render(article.content)"></div>
+    <v-row>
+      <v-col cols="12">
+        <span class="text-h2">{{ article.title }}</span>
+      </v-col>
+    </v-row>
+    <v-divider class="my-8"></v-divider>
+    <v-row>
+      <v-col cols="12">
+        <div v-html="this.$md.render(article.content)"></div>
+      </v-col>
+    </v-row>
   </v-container>
 </template>
 
@@ -10,7 +20,9 @@ import articleQuery from "~/apollo/queries/article/article";
 export default {
   data() {
     return {
-      article: {},
+      article: {
+        content: "",
+      },
     };
   },
   apollo: {
--- pages/articles/index.vue
+++ pages/articles/index.vue
@@ -1,5 +1,37 @@
 <template>
-  <v-container>
-    <h1>Articles page</h1>
+  <v-container class="flex flex-wrap">
+    <v-card v-for="article in articles" :key="article.id">
+      <v-img
+        :src="`${apiUrl}${article.image.formats.small.url}`"
+        height="180"
+      ></v-img>
+      <v-card-title class="text-h3"> {{ article.title }} </v-card-title>
+      <v-card-text>
+        <nuxt-link
+          class="grey--text darken-2"
+          :to="{ name: 'articles-id', params: { id: article.id } }"
+          >Read more</nuxt-link
+        >
+      </v-card-text>
+    </v-card>
   </v-container>
 </template>
+
+<script>
+import articlesQuery from "~/apollo/queries/article/articles";
+
+export default {
+  data() {
+    return {
+      apiUrl: process.env.strapiBaseUri,
+      articles: [],
+    };
+  },
+  apollo: {
+    articles: {
+      prefetch: true,
+      query: articlesQuery,
+    },
+  },
+};
+</script>
